75c8fa89b6c8e1de57eb107842d8255dcdd4dd48
aburbank <ashlynburbank@gmail.com>
2020-09-14 16:10:37 -0500

    finished exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dolly", false)]
+        public void IsAPalindrome(string word, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
+
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
